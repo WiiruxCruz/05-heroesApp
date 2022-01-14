af07686ed87e259e5246aa80abb488c8939d7181
WiiruxCruz <hialf.92@gmail.com>
2022-01-14 01:05:11 -0600

    Creación de interfaz de heroes e impl de listado

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-listado',
@@ -9,12 +10,17 @@ import { HeroesService } from '../../services/heroes.service';
 })
 export class ListadoComponent implements OnInit {
 
+  heroes: Heroe[] = [];
+
   constructor( private heroesService: HeroesService ) { }
 
   ngOnInit(): void {
     this.heroesService.getHeroes()
     .subscribe(
-      resp => console.log( resp )
+      //resp => console.log( resp )
+      resp => {
+        this.heroes = resp;
+      }
     );
   }
 
