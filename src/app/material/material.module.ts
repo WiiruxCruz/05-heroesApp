f3eead74ab137459127633146adb06c0caeb1859
WiiruxCruz <hialf.92@gmail.com>
2022-01-21 14:34:37 -0600

    Creación de formulario agregar personaje

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -11,6 +11,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import {MatInputModule} from '@angular/material/input';
     MatProgressSpinnerModule,
     MatAutocompleteModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule
   ]
 })
 export class MaterialModule { }
