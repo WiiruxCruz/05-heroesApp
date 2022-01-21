bd540e011eac07f886b7cbbb71539669607be538
WiiruxCruz <hialf.92@gmail.com>
2022-01-20 22:52:51 -0600

    Diseño de pantalla de heroe con servicio

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { switchMap } from 'rxjs/operators';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
@@ -9,12 +12,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor( private activateRoute: ActivatedRoute) { }
+  heroe!: Heroe;
+
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private heroesService: HeroesService
+  ) { }
 
   ngOnInit(): void {
     this.activateRoute.params
+    .pipe(
+      switchMap( ({ id }) => this.heroesService.getHeroePorId( id ) )
+    )
     .subscribe(
-      ({ id }) => console.log( id )
+      //({ id }) => console.log( id )
+      heroe => this.heroe = heroe
     );
     //id del heroe
     //y mostrarlo en consola
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,6 +6,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 
 @NgModule({
@@ -16,7 +18,9 @@ import {MatCardModule} from '@angular/material/card';
     MatButtonModule,
     MatIconModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatGridListModule,
+    MatProgressSpinnerModule
   ]
 })
 export class MaterialModule { }
