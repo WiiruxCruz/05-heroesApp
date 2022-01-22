1bcd1bb2de4a445d711427e8c3f7ea0ec6ed12ed
WiiruxCruz <hialf.92@gmail.com>
2022-01-21 18:47:47 -0600

    Manejo de excepciones de imagenes

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -7,8 +7,12 @@ import { switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [`
+    img {
+      width: 100%;
+      border-radius: 5px;
+    }
+  `]
 })
 export class AgregarComponent implements OnInit {
   publishers = [
@@ -40,6 +44,11 @@ export class AgregarComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
+    if( !this.router.url.includes('editar') ) {
+      return;
+    }
+
     this.activatedRoute.params
     .pipe(
       switchMap( ({ id }) => this.heroesService.getHeroePorId( id ) )
