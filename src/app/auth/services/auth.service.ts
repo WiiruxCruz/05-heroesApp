e5411330cda4d5323523823fa583aad3744834b6
WiiruxCruz <hialf.92@gmail.com>
2022-01-26 17:40:19 -0600

    Mostrar información del usuario activo

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth.interface';
 
@@ -9,10 +10,21 @@ import { Auth } from '../interfaces/auth.interface';
 export class AuthService {
 
   private baseUrl: string = environment.baseUrl;
+  private _auth: Auth | undefined;
+
+  get auth(): Auth {
+    return { ...this._auth! }
+  }
 
   constructor( private http: HttpClient ) { }
 
   login() {
-    return this.http.get<Auth>(`${ this.baseUrl }/usuarios/1`);
+    return this.http.get<Auth>(`${ this.baseUrl }/usuarios/1`)
+    .pipe(
+      tap(
+        //resp => console.log('AUTHSERVICE', resp)
+        auth => this._auth = auth
+      )
+    );
   }
 }
