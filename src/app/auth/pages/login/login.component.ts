12927f3b6b3548f7a1e0c7604bbbe7c0ab84a3b4
WiiruxCruz <hialf.92@gmail.com>
2022-01-26 17:15:37 -0600

    Servicio para mantener el estado de auth

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,13 +11,26 @@ import { Router } from '@angular/router';
 export class LoginComponent {
 
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { }
 
   login() {
     //Ir al backend
     // un usuario
-    this.router.navigate(['./heroes']);
+
+    this.authService.login()
+    .subscribe(
+      resp => {
+        console.log(resp);
+
+        if( resp.id ) {
+          this.router.navigate(['./heroes']);
+        }
+      }
+    );
+
+    //this.router.navigate(['./heroes']);
   }
 
 }
